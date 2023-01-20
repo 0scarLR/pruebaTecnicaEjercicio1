234826c7551dbd037383228598d59e1643c3de6d
OscarLR <oscarlopezjjo@gmail.com>
2023-01-20 16:06:56 -0600

    header listo

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,14 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
