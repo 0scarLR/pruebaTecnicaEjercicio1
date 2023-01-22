e5071170120e79f48a86d0b622850f283d79cc51
OscarLR <oscarlopezjjo@gmail.com>
2023-01-21 20:58:07 -0600

    login funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './guard/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 import { PagesComponent } from './pages/pages.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
 
+    canActivate: [AuthGuard],
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  public formSubmitted = false;
+
+  public loginForm = this.fb.group({
+    name: ['ash', Validators.required],
+    password: ['123456', Validators.required],
+  });
+
+  constructor(private router: Router, private fb: FormBuilder) {}
+  login() {
+    if (
+      this.loginForm.value.name === 'ash' &&
+      this.loginForm.value.password === '123456'
+    ) {
+      this.router.navigate(['/dashboard']);
+    } else {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Credenciales invalidas',
+        icon: 'error',
+        confirmButtonText: 'regresar',
+      });
+    }
+  }
 
   ngOnInit(): void {}
 }
